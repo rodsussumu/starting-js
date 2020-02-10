38cd98cd1b47e34ce861f1daca6e03895abe44e6
Rodrigo Tanaka <rstanaka@aluscseduca.com.br>
2020-02-10 17:27:11 -0300

    Atualização

---
+++ aula6 - Operadores Aritmeticos/index.js
@@ -0,0 +1,14 @@
+/* Operadores aritmeticos 
++ Adição / Concatenação 
+- Subtração
+* Multiplicação
+/ Divisão
+% Resto da divisão
+++ Incrementaçao
+x= Atribuição de valores
+** Potenciação
+*/
+
+let num1 = 5;
+let num2 = 10;
+console.log(num1 ** num2);
