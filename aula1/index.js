e65bea25191178392268bdedb2c53189787d1ef0
Rodrigo Tanaka <rstanaka@aluscseduca.com.br>
2020-02-10 17:14:29 -0300

    Dia 10/02

--- aula1/index.js
+++ aula1/index.js
@@ -1,22 +1,3 @@
 console.log('Hello World'); // String
 console.log(35, 15.85, 'Rodrigo Tanaka'); // Number
-console.log('Meu nome é "Rodrigo". Estou aprendendo javascript as', 10 ,'da noite.')
-
-
-var funcs = [];
-for (var i = 0; i < 3; i++) {
-    funcs[i] = () => console.log("Valor: " + i);
-}
-for (var j = 0; j < 3; j++) {
-    funcs[j]();
-}
-
-var funcs = [];
-for (let i = 0; i < 3; i++) {
-    funcs[i] = () => console.log("Valor: " + i);
-}
-for (let j = 0; j < 3; j++) {
-    funcs[j]();
-}
-
-console.log(i);
+console.log('Meu nome é "Rodrigo". Estou aprendendo javascript as', 10 ,'da noite.');
