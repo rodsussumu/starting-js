6604de163cac3a705d56ea61449f902479e62d0c
Rodrigo Tanaka <rstanaka@aluscseduca.com.br>
2020-02-10 17:13:33 -0300

    dia 10/02

---
+++ aula4/index.js
@@ -0,0 +1,6 @@
+const primeiroNumero = 5; // Não é possivel modificar o valor
+const segundoNumero = 10; // Necessita de um valor para inicialização
+const conta = primeiroNumero * segundoNumero;
+const contaDuplicado = conta * 2;
+console.log(conta);
+console.log(typeof(primeiroNumero));
---
+++ aula5/index.js
@@ -0,0 +1,11 @@
+const nome = "Rodrigo Sussumu";
+const sobrenome = "Tanaka";
+const idade = 20;
+const peso = 84;
+const altura = 1.80;
+let imc = peso / (altura * altura);
+let anoNascimento = 2020 - idade;
+
+console.log(imc);
+console.log(anoNascimento);
+console.log(`Meu nome é ${nome}`);
