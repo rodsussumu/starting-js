a220675487bf1e86640fdbaedff9a7a73a5ee69d
Rodrigo Tanaka <rstanaka@aluscseduca.com.br>
2020-02-13 16:40:14 -0300

    dia 13/02

---
+++ aula11 - Number/index.js
@@ -0,0 +1,14 @@
+// let num1 = 2;
+// let num2 = 3;
+// let num3 = 10.59645956165
+// console.log(num1.toString() + num2);  converte para string
+// console.log(num3.toFixed(2));  define as casas decimais   
+// console.log(Number.isInteger(num2));  verifica se o número é um inteiro
+
+let num4 = 0.7;
+let num5 = 0.1;
+//console.log((num4 + num5) * 100 / 100) ;
+// ou
+num4 += num5;
+console.log(parseFloat(num4.toFixed(2)));
+//console.log(parseFloat(num4 + num5).toFixed(1));
