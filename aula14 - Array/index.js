0006beaad79a0dfac76b1ea27a57db784609a8d0
Rodrigo Tanaka <rstanaka@aluscseduca.com.br>
2020-02-17 01:02:41 -0300

    aula 16/02

---
+++ aula14 - Array/index.js
@@ -0,0 +1,7 @@
+const alunos = ['rodrigo', 'iago'];
+alunos.push("abraao"); // Adiciona elementos no final
+alunos.unshift("geovanne"); // Adiciona elementos no começo
+// alunos.pop(); // Remova elementos do final
+// alunos.shift(); // Remove do começo
+// console.log(alunos.slice(0, 2)); Retorna de determinada distancia
+console.log(alunos.slice(0, 2));
---
+++ aula15 - Funções/index.js
@@ -0,0 +1,19 @@
+// function saudacao(nome) {
+//     return `Bom dia ${nome}`;
+// }
+
+// const valor = saudacao('Rodrigo');
+// console.log(valor);
+
+// function soma(x, y) {
+//     const resultado = x + y;
+//     return resultado;
+// }
+// console.log(soma(4, 5));
+
+const raiz = (n) => {
+    return n ** 0.5
+}
+
+console.log(raiz(9));
+
